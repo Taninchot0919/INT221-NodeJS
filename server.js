3af32902b8b021eff2ccacb0f65eb210748c1126
Taninchot Phuwaloertthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-08-01 17:25:16 +0700

    [UPDATE] Move to prisma framework and user can use Insert,SELECT

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ let app = express();
 let bodyParser = require("body-parser")
 let mysql = require("mysql")
 let moment = require("moment")
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -27,71 +29,106 @@ app.get("/", (req, res) => {
 })
 
 // Get all albums
-app.get("/albums", (req, res) => {
-    dbConnect.query("SELECT * FROM ALBUM", (error, results, field) => {
-        if (error) {
-            throw error
+app.get("/albums", async (req, res) => {
+    const allAlbums = await prisma.album.findMany({
+        include: {
+            artist: true,
+            albumType: true,
+            album_details: true
         }
-        let message = "";
-        if (results.length == 0) {
-            message = "Table is empty"
-            return res.send({ error: true, message: message })
-        }
-        // Front-End ต้องไปแปลง date เอง
-        results.forEach(result => {
-            result.release_date = moment(result.release_date).format("YYYY-MM-DD")
-            // console.log(result.release_date)
-        });
-        return res.send({ data: results })
     })
+    allAlbums.forEach(album => {
+        album.release_date = moment(album.release_date).format("YYYY-MM-DD")
+    });
+    return res.send({ data: allAlbums })
 })
 
-// เอาไว้ Query
-function asyncQuery(sqlQuery) {
-    return new Promise((resolve, reject) => {
-        dbConnect.query(sqlQuery, function (err, result) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        })
-    })
-}
+app.post("/album", async (req, res) => {
+    let albumName = req.body.album_name
+    let price = req.body.price
+    let releaseDate = req.body.release_date
+    let description = req.body.description
+    let coverImage = req.body.cover_image
+    let artistId = req.body.art_id
+    let albumTypeId = req.body.at_id
+    let albumDetails = req.body.album_details
 
-async function getMaxId() {
-    let querySQL = "SELECT MAX(a_id) as maxId from album";
-    let maxId = await asyncQuery(querySQL);
-    let maxNumber = await maxId[0].maxId.split("al")
+    let date = new Date(releaseDate)
+    date.setDate(date.getDate())
+    releaseDate = moment(date).format()
+
+    let maxNumber = maxAlbumId._max.a_id.split("al")
     maxNumber = parseInt(maxNumber[1]) + 1
-    let albumId = "al" + maxNumber
-    return albumId;
-}
+    maxAlbumId = "al" + maxNumber
 
+    let maxAlbumId = await prisma.album.aggregate({
+        _max: {
+            a_id: true
+        }
+    })
 
-app.post("/add", async (req, res) => {
-    let albumId = await getMaxId();
-    console.log(albumId)
-    let albumName = req.body.album_name;
-    let price = req.body.price;
-    let releaseDate = req.body.release_date;
-    let description = req.body.description;
-    let coverImage = req.body.cover_image;
-    let artistId = req.body.art_id;
-    let albumTypeId = req.body.at_id;
-
-    releaseDate = moment(releaseDate).format("YYYY-MM-DD")
-
-    if (!albumName && !price && !releaseDate && !description && !artistId && !albumTypeId) {
-        return res.status(400).send({ error: true, message: "Please fill all form" })
-    }
-
-    dbConnect.query('INSERT INTO album VALUES (?,?,?,?,?,?,?,?)', [albumId, albumName, price, releaseDate, description, coverImage, artistId, albumTypeId],
-        (error, results, field) => {
-            if (error) {
-                throw error
-            }
-            return res.send({ error: false, data: results, message: "Successfully Added!!!" })
-        })
+    const addAlbum = await prisma.album.create({
+        data: {
+            a_id: maxAlbumId,
+            album_name: albumName,
+            price: price,
+            release_date: releaseDate,
+            description: description,
+            cover_image: coverImage,
+            art_id: artistId,
+            at_id: albumTypeId,
+            album_details: { create: albumDetails }
+        }
+    })
+    return res.send({ addAlbum })
 })
+
+// // เอาไว้ Query
+// function asyncQuery(sqlQuery) {
+//     return new Promise((resolve, reject) => {
+//         dbConnect.query(sqlQuery, function (err, result) {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(result);
+//             }
+//         })
+//     })
+// }
+
+// async function getMaxId() {
+//     let querySQL = "SELECT MAX(a_id) as maxId from album";
+//     let maxId = await asyncQuery(querySQL);
+//     let maxNumber = await maxId[0].maxId.split("al")
+//     maxNumber = parseInt(maxNumber[1]) + 1
+//     let albumId = "al" + maxNumber
+//     return albumId;
+// }
+
+
+// app.post("/add", async (req, res) => {
+//     let albumId = await getMaxId();
+//     console.log(albumId)
+//     let albumName = req.body.album_name;
+//     let price = req.body.price;
+//     let releaseDate = req.body.release_date;
+//     let description = req.body.description;
+//     let coverImage = req.body.cover_image;
+//     let artistId = req.body.art_id;
+//     let albumTypeId = req.body.at_id;
+
+//     releaseDate = moment(releaseDate).format("YYYY-MM-DD")
+
+//     if (!albumName && !price && !releaseDate && !description && !artistId && !albumTypeId) {
+//         return res.status(400).send({ error: true, message: "Please fill all form" })
+//     }
+
+//     dbConnect.query('INSERT INTO album VALUES (?,?,?,?,?,?,?,?)', [albumId, albumName, price, releaseDate, description, coverImage, artistId, albumTypeId],
+//         (error, results, field) => {
+//             if (error) {
+//                 throw error
+//             }
+//             return res.send({ error: false, data: results, message: "Successfully Added!!!" })
+//         })
+// })
 module.exports = app
