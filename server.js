e5a6be92103696bb7c8ee684eb6f70f0786e9400
Taninchot Phuwaloertthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-08-03 20:35:39 +0700

    [UPDATE] Clean code a bit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 let express = require("express")
-let app = express();
+let app = express()
 // let bodyParser = require("body-parser")
 // let mysql = require("mysql")
 let moment = require("moment")
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 let multer = require("multer")
 let path = require("path")
-let fs = require("fs");
+let fs = require("fs")
 let filePath = path.join(__dirname + "/uploads/test.json")
 
 app.use(express.json())
@@ -18,18 +18,29 @@ app.use(express.urlencoded({ extended: true }))
 const storage = multer.diskStorage({
     destination: './uploads/',
     filename: (req, file, callback) => {
-        callback(null, file.originalname);
+        callback(null, file.originalname)
     }
 })
 
+// Connect To MySQL
+/* let dbConnect = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "album_shop_test"
+})
+
+dbConnect.connect() */
+
+
 // Function for check file type
 function checkFileType(file, callback) {
-    const filetypes = /jpeg|jpg|png|gif|json/;
+    const filetypes = /jpeg|jpg|png|gif|json/
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
     const mimetype = filetypes.test(file.mimetype)
 
     if (mimetype && extname) {
-        return callback(null, true);
+        return callback(null, true)
     } else {
         callback('error: image only')
     }
@@ -63,58 +74,18 @@ app.post("/test", (req, res) => {
                     reject(err)
                 }
             })
-        }).then(async (data) => {
-            let albumName = data.album_name
-            let price = data.price
-            let releaseDate = data.release_date
-            let description = data.description
-            let coverImage = req.files[0].originalname
-            let artistId = data.art_id
-            let albumTypeId = data.at_id
-            let albumDetails = data.album_details
-
-            let date = new Date(releaseDate)
-            date.setDate(date.getDate())
-            releaseDate = moment(date).format()
-
-            let maxAlbumId = await prisma.album.aggregate({
-                _max: {
-                    a_id: true
-                }
-            })
-
-            let maxNumber = maxAlbumId._max.a_id.split("al")
-            maxNumber = parseInt(maxNumber[1]) + 1
-            maxAlbumId = "al" + maxNumber
-
-            await prisma.album.create({
-                data: {
-                    a_id: maxAlbumId,
-                    album_name: albumName,
-                    price: price,
-                    release_date: releaseDate,
-                    description: description,
-                    cover_image: coverImage,
-                    art_id: artistId,
-                    at_id: albumTypeId,
-                    album_details: { create: albumDetails }
-                }
+        }).then((coverJsonValue) => {
+            addAlbum(coverJsonValue).then((value) => {
+                return res.send(value)
+            }).catch((err) => {
+                // console.log(err)
+                // err = "Error"
+                return res.send({ status: "Error Something" })
             })
         })
-    }).then(() => {
-        return res.send({ status: "Done" })
     })
 })
 
-// Connect To MySQL
-/* let dbConnect = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "album_shop_test"
-})
-
-dbConnect.connect(); */
 
 app.listen(9000, () => {
     console.log("Running at port : 9000")
@@ -125,10 +96,8 @@ app.get("/", (req, res) => {
     return res.send({ error: false, message: "Welcome to rest api with Node JS", written_by: "Taninchot Phuwaloertthiwat" })
 })
 
-
 // Get all albums
 app.get("/albums", async (req, res) => {
-    // let allAlbums = await prisma.$queryRaw("SELECT * FROM ALBUM")
     let allAlbums = await prisma.album.findMany({
         include: {
             artist: true,
@@ -138,10 +107,11 @@ app.get("/albums", async (req, res) => {
     })
     allAlbums.forEach(album => {
         album.release_date = moment(album.release_date).format("YYYY-MM-DD")
-    });
+    })
     return res.send({ data: allAlbums })
 })
 
+// Delete Album from id 
 app.delete("/album/:id", async (req, res) => {
     let id = req.params.id
     console.log(id)
@@ -163,18 +133,9 @@ app.delete("/album/:id", async (req, res) => {
 
 app.put("/album/:id", (req, res) => {
     let id = req.params.id
-    let albumName = req.body.album_name
-    let price = req.body.price
-    let releaseDate = req.body.release_date
-    let description = req.body.description
-    let coverImage = req.body.cover_image
-    let artistId = req.body.art_id
-    let albumTypeId = req.body.at_id
-    let albumDetails = req.body.album_details
+    let { albumName, price, releaseDate, description, coverImage, artistId, albumTypeId, albumDetails } = req.body
 
-    let date = new Date(releaseDate)
-    date.setDate(date.getDate())
-    releaseDate = moment(date).format()
+    releaseDate = formatDate(releaseDate)
 
     prisma.album.update({
         where: {
@@ -206,50 +167,65 @@ app.put("/album/:id", (req, res) => {
             }).then((result) => {
                 console.log(result)
             })
-        });
+        })
     }).then(() => {
         return res.send({ message: "Update Successfully" })
     })
 })
 
 app.post("/album", async (req, res) => {
-    let albumName = req.body.album_name
-    let price = req.body.price
-    let releaseDate = req.body.release_date
-    let description = req.body.description
-    let coverImage = req.body.cover_image
-    let artistId = req.body.art_id
-    let albumTypeId = req.body.at_id
-    let albumDetails = req.body.album_details
+    let album = req.body
 
-    let date = new Date(releaseDate)
-    date.setDate(date.getDate())
-    releaseDate = moment(date).format()
-
-    let maxAlbumId = await prisma.album.aggregate({
-        _max: {
-            a_id: true
-        }
+    addAlbum(album).then((value) => {
+        res.send(value)
+    }).catch((err) => {
+        err = "Error Something"
+        res.send({ status: err })
     })
-
-    let maxNumber = maxAlbumId._max.a_id.split("al")
-    maxNumber = parseInt(maxNumber[1]) + 1
-    maxAlbumId = "al" + maxNumber
-
-    const addAlbum = await prisma.album.create({
-        data: {
-            a_id: maxAlbumId,
-            album_name: albumName,
-            price: price,
-            release_date: releaseDate,
-            description: description,
-            cover_image: coverImage,
-            art_id: artistId,
-            at_id: albumTypeId,
-            album_details: { create: albumDetails }
-        }
-    })
-    return res.send({ addAlbum })
 })
 
+// Format Date Function
+function formatDate(oldDate) {
+    let date = new Date(oldDate)
+    date.setDate(date.getDate())
+    return oldDate = moment(date).format()
+}
+
+async function addAlbum(albumData) {
+    try {
+        let { albumName, price, releaseDate, description, coverImage, artistId, albumTypeId, albumDetails } = albumData
+        releaseDate = formatDate(releaseDate)
+
+        let maxAlbumId = await prisma.album.aggregate({
+            _max: {
+                a_id: true
+            }
+        })
+
+        let maxNumber = maxAlbumId._max.a_id.split("al")
+        maxNumber = parseInt(maxNumber[1]) + 1
+        maxAlbumId = "al" + maxNumber
+
+        if (!albumName && !price && !releaseDate && !description && !coverImage && !artistId && !albumTypeId && !albumDetails) {
+            throw new Error("Invalid Data")
+        }
+
+        return await prisma.album.create({
+            data: {
+                a_id: maxAlbumId,
+                album_name: albumName,
+                price: price,
+                release_date: releaseDate,
+                description: description,
+                cover_image: coverImage,
+                art_id: artistId,
+                at_id: albumTypeId,
+                album_details: { create: albumDetails }
+            }
+        })
+    } catch (error) {
+        err = "Invalid Data"
+        throw new Error(error)
+    }
+}
 module.exports = app
